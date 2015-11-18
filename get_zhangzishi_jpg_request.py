f4ba6c23e8555fa830bbff51ccc9c733ff51be5b
zhouyuzai110 <zhouyuzai110@gmail.com>
2015-11-18 20:29:36 +0800

    update get_zhangzishi_jpg_request.py

--- get_zhangzishi_jpg_request.py
+++ get_zhangzishi_jpg_request.py
@@ -7,9 +7,7 @@ import codecs
 
 import time, os
 
-url = raw_input("the url is :")
-zi_fu_chuan = ["imageView", "weixin", "330x200", "600x120", "footer"]
-r = requests.get(url)
+
 
 # #u"获取源码中得超链接"
 # def get_hyper_links(url, key_word):
@@ -29,16 +27,18 @@ r = requests.get(url)
 
 #u"获取源码中得超链接"
 def get_hyper_links(url, key_word):
+
     try:
     	links = []
-        soup = BeautifulSoup(r.content)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content,"html.parser")
         p = soup.find_all('img')
         for i in p:
             target_link = i.get('src')
             if target_link is not None :
                 if target_link.find(key_word) > 0:
                     if "imageView" not in target_link and "weixin" not in target_link\
-                    and "330x200" not in target_link and "600x120" not in target_link \
+                    and "330x200" not in target_link and "600x120" not in target_link\
                     and "footer" not in target_link:
                     	links.append(target_link) 
         return links
@@ -46,13 +46,34 @@ def get_hyper_links(url, key_word):
         print str(e)
         return None          
 
+def write_into_files(url):
+
+    conn = urllib.urlopen(url) 
+    jpgname = str(time.time()) + ".jpg"
+    f = open(jpgname,'wb')  
+    f.write(conn.read())  
+    f.close()  
+
+
+def main():
+
+    while True:
+        os.chdir("/home/evas/")
+        url = raw_input("the url is :")
+        links = get_hyper_links(url,"jpg")
+        for link in links:
+            try:
+                write_into_files(link)
+                print('Pic Saved!') 
+            except Exception, e:
+                print str(e)
+
+        
+
+
 
 if __name__ == "__main__":
-	os.chdir(u"D:\我的文档\Dropbox\Photo")  
-	for jpg_links in get_hyper_links(url, "jpg"):
-		conn = urllib.urlopen(jpg_links) 
-		jpgname = str(time.time()) + ".jpg"
-		f = open(jpgname,'wb')  
-		f.write(conn.read())  
-		f.close()  
-		print('Pic Saved!')         
+
+    main()  
+	
+	

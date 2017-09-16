929de7e9c91afa62061e7332066422fce8b0881f
zhouyuzai110 <zhouyuzai110@gmail.com>
2017-09-16 20:07:38 +0800

    add get_toutiao_jpg_request.py and modified get_zhangzishi_jpg_request.py

--- get_toutiao_jpg_request.py
+++ get_toutiao_jpg_request.py
@@ -4,41 +4,41 @@
 from bs4 import BeautifulSoup
 import requests
 import codecs
-
+# from htmllib import HTMLParser 
+import HTMLParser
 import time, os
-
+import re
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'}
+
+pattern = re.compile(r'http://.{2,3}.pstatp.com/large/.{20}')
+# pattern = re.compile(r'pstatp')
 
    
 
 #u"获取源码中得超链接"
-def get_hyper_links(url, key_word):
+def get_hyper_links(url):
 
     try:
         links = []
         r = requests.get(url, headers = headers)
-        soup = BeautifulSoup(r.content,"html.parser")
-        p = soup.find_all('img')
+        html_parser = HTMLParser.HTMLParser()
+        txt = html_parser.unescape(r.content.decode('utf-8'))
+        soup = BeautifulSoup(txt, "html.parser")
+        p = soup.find_all('script')
         for i in p:
-            target_link = i.get('src')
-            if target_link is not None :
-                if target_link.find(key_word) > 0:
-                    if "imageView" not in target_link and "weixin" not in target_link\
-                    and "330x200" not in target_link and "600x120" not in target_link\
-                    and "footer" not in target_link and '&w=240&h=180' not in target_link\
-                    and '&w=120&h=120' not in target_link and '&w=100&h=75' not in target_link\
-                    and 'qrcode_for_gh' not in target_link:
-                        links.append(target_link) 
+            if "articleInfo:" in i.get_text():
+                links = re.findall(pattern, i.get_text())
         return links
     except Exception,e:
         print str(e)
         return None          
 
+
 def get_title(url):
     r = requests.get(url, headers = headers)
     soup = BeautifulSoup(r.content,"html.parser")
-    title = soup.find('h1', {'class':"article-title"}).get_text()
+    title = soup.find('title').get_text()
     return title
 
 
@@ -57,7 +57,7 @@ def main():
     while True:
         os.chdir("/home/evas/Dropbox/Photo/")
         url = raw_input("the url is : ")
-        links = get_hyper_links(url,"jpg")
+        links = get_hyper_links(url)
         title = get_title(url)
         total_num = len(links)
         num = 1
@@ -67,7 +67,7 @@ def main():
                 jpgname = title + str(time.time()) + ".jpg"
                 write_into_files(jpgname,link)
                 print('%s of %s Pic Saved!') %(num, total_num)
-                num += 1 
+                num += 1
             except Exception, e:
                 print str(e)
 

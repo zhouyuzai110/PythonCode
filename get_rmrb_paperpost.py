f30bfc8b39e8a70b5274a3051a0d31a44448f33f
zhouyuzai110 <zhouyuzai110@gmail.com>
2018-10-18 19:04:24 +0800

    modified:   get_rmrb_paperpost.py

--- get_rmrb_paperpost.py
+++ get_rmrb_paperpost.py
@@ -7,6 +7,7 @@ import codecs
 import HTMLParser
 import time
 import sys
+import itchat
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -25,7 +26,6 @@ def create_hyper_links_rmrb():
             rmrb_day = "0" + rmrb_day
         rmrb_date = rmrb_year + "-" + rmrb_month + "-" + rmrb_day
         rmrb_link = "http://paperpost.people.com.cn/all-rmrb-" + rmrb_date + ".html"
-        print rmrb_link
         return rmrb_link
     else:
         rmrb_link = "http://paperpost.people.com.cn/all-rmrb-" + sys.argv[1] + ".html"
@@ -33,19 +33,18 @@ def create_hyper_links_rmrb():
    
 
 #u"获取源码中得超链接"
-def get_hyper_links_rmrb(url):
+def get_hyper_links_rmrb():
 
+    url = create_hyper_links_rmrb()
     try:
         r = requests.get(url, headers = headers)
-        # html_parser = HTMLParser.HTMLParser()
-        # txt = html_parser.unescape(r.content.decode('utf-8'))
         soup = BeautifulSoup(r.content, "html.parser")
         p = soup.find_all('a')
         links = []
         
         for i in p:
-            rmrb_article = i.get_text()
-            rmrb_link = i.get('href')
+            rmrb_article = i.get_text().replace("\r\n", "")
+            rmrb_link = get_short_url(i.get('href'))
             rmrb_article_link = rmrb_article + '\n' + rmrb_link
             links.append(rmrb_article_link)
         return links
@@ -53,14 +52,14 @@ def get_hyper_links_rmrb(url):
             print str(e)
 
 
-def write_into_files(url):
+def write_into_files():
     
     try: 
-        links = get_hyper_links_rmrb(url)
+        links = get_hyper_links_rmrb()
         for i in links:
             f = open("rmrb",'a')
-            f.write("\n")  
-            f.write(i) 
+            f.write(i)
+            f.write("\n") 
             f.close()  
 
     except Exception,e:
@@ -68,13 +67,42 @@ def write_into_files(url):
         return None
 
 
+def itchat_send():
+
+    sms = get_hyper_links_rmrb()
+    sms_text = "\n".join(sms)
+    itchat.auto_login()
+    name = itchat.search_friends(name = 'Mao')
+    Mao = name[0]["UserName"]
+    itchat.send_msg(sms_text, toUserName = Mao)
+
+
+def get_short_url(url):
+    """
+    获取百度短网址
+    @param url: {str} 需要转换的网址
+    @return: {str} 成功：转换之后的短网址，失败：原网址
+    """
+    api = "http://dwz.cn/admin/create"
+    data = {
+        "url": url
+    }
+    response = requests.post(api, json=data)
+    if response.status_code != 200:
+        return url
+    result = response.json()
+    code = result.get("Code")
+    if code == 0:
+        return result.get("ShortUrl")
+    else:
+        return url
+
+
 def main():
 
     try:
-        rmrb_url = create_hyper_links_rmrb()
-        # rmrb_url = "http://paperpost.people.com.cn/all-rmrb-2018-10-17.html"
-        write_into_files(rmrb_url)
-
+        write_into_files()
+        itchat_send()
     except Exception, e:
         print str(e)
 
